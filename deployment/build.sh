3ff4e6682335a4be51bdb1b7229f78d3ea3ea366
costa <constant9@gmail.com>
2018-02-21 23:52:32 +0200

    edited the build script to run full cycle : build docker, build mvn app, run jar

--- deployment/build.sh
+++ deployment/build.sh
@@ -2,7 +2,7 @@
 cd mysql
 docker build -t mysqlimage_costa .
 docker run -d -p 4406:3306 mysqlimage_costa
-cd ..
+cd ../..
+mvn clean install
+cd microblog-app/target/
 java -jar microblog-app-1.0-SNAPSHOT.jar
-
-
